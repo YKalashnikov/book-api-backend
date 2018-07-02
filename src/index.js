f21af0333307477971d9e80d4f98933a987dc782
YKalashnikov <uk2709@gmail.com>
2018-07-02 00:15:54 -0700

    back end

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import Promise from "bluebird";
+import auth from "./routes/auth";
+import users from "./routes/users";
+import books from "./routes/books"
 
+
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL);
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({errors: {global:"Invalid credentials"}})
-})
-
-app.get('/*',(req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
-})
+app.use("/api/auth", auth);
+app.use("/api/users", users);
+app.use("/api/users", books)
 
 
-app.listen(8080, () => 
-console.log('Running on local host 8080'))
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8080, () => console.log("Running on localhost:8080"));
